60fd017ea1eb63f10c7dc1f9b878381a617ed7f6
Anjelika <fominaangelika86@gmail.com>
2021-10-22 11:47:10 +0300

    Fix Component Calendar

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { DateContext } from "../../contexts";
 import styles from "./Calendar.module.scss";
 import SectionCalendar from "./SectionCalendar";
 import SectionCurrentDayAndDayWeek from "./SectionCurrentDayAndDayWeek";
 
 function Calendar() {
+  const [currentDay, setCurrentDay] = useState(new Date());
+
   return (
-    <article className={styles.container}>
-      <SectionCurrentDayAndDayWeek />
-      <SectionCalendar />
-    </article>
+    <DateContext.Provider value={[currentDay, setCurrentDay]}>
+      <article className={styles.container}>
+        <SectionCurrentDayAndDayWeek />
+        <SectionCalendar />
+      </article>
+    </DateContext.Provider>
   );
 }
 export default Calendar;
