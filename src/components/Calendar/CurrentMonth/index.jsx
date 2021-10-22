84c7788f7a1702ee1eb378caf70af84359387f9b
Anjelika <fominaangelika86@gmail.com>
2021-10-22 11:49:45 +0300

    Fix CurrentMonth

--- src/components/Calendar/CurrentMonth/index.jsx
+++ src/components/Calendar/CurrentMonth/index.jsx
@@ -1,7 +1,18 @@
-import { useEffect, useState } from "react";
-import { format, add } from "date-fns";
+import React, { useContext } from "react";
+import { getWeeksInMonth, addDays, startOfMonth } from "date-fns";
+import styles from "./CurrentMonth.module.scss";
+import { DateContext } from "../../../contexts";
+import Week from "./../../Calendar/Week";
 
-function CurrentMonth() {
-  return;
+function CurrentMonth(props) {
+  const [currentDay] = useContext(DateContext);
+  const startingDate = startOfMonth(currentDay);
+  const weekAmount = getWeeksInMonth(currentDay);
+
+  const weeksArr = new Array(weekAmount).fill(null).map((_, index) => {
+    return <Week key={index} currentDay={addDays(startingDate, index * 7)} />;
+  });
+
+  return <div className={styles.week}>{weeksArr}</div>;
 }
 export default CurrentMonth;
